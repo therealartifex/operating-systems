97656274281b406766105cbb8936f32b7b789d1d
Sceleris Artifex <linux.usaf@live.com>
2016-02-03 20:56:42 -0600

    add code to replace whitespace with null terminator

--- unix_shell/BJS_wsh.c
+++ unix_shell/BJS_wsh.c
@@ -8,19 +8,25 @@ Assignment 3 - "Simple UNIX Shell"
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <ctype.h>
 
-#define M_LIN 80
+#define M_LIN 256
 
 int main(void)
 {
-	char *args[M_LIN/2 + 1];
+	char *args[M_LIN/2 + 1]; // args go here
+	char *buf[M_LIN]; // complete command goes here
 	int run = 1;
 	
+	// loop till we kill it
 	while (run) {
 		printf("wsh>");
 		fflush(stdout);
 		
-		
+		fgets(buf, M_LIN, stdin); // read the command
+		for(i=0;i<strlen(buf);i++) if(isspace(buf[i])) a[i] = '\0';
+		printf("%s\n",buf);
 	}
 	return 0;
 }
