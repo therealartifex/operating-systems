0412d1b23d8e31603fe6449164fef5b7fe5a32cd
Sceleris Artifex <linux.usaf@live.com>
2016-02-03 21:17:59 -0600

    set args pointing to each argument in buf[]

--- unix_shell/BJS_wsh.c
+++ unix_shell/BJS_wsh.c
@@ -22,13 +22,22 @@ int main(void)
 	// loop till we kill it
 	while (run) {
 		int i;
+		int argc;
 		
-		printf("wsh>");
+		printf("wsh> "); // wsh = weird shell
 		fflush(stdout);
 		
 		fgets(buf, M_LIN, stdin); // read the command
 		
-		for(i=0;i<strlen(buf);i++) if(isspace(buf[i])) buf[i] = '\0';
+		args[0]=&buf[0]; // first command is first argument
+		
+		for(i=0;i<strlen(buf);i++) {
+			if(isspace(buf[i])){
+				 buf[i] = '\0';
+				 args[++argc]=&buf[i+1];
+			}
+		}
+			
 		printf("%s\n",buf);
 	}
 	return 0;
