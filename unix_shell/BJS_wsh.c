cfe647dec91e926f56e36b193d8591c95cdddbce
therealartifex <linux.usaf@live.com>
2016-02-04 05:03:11 -0600

    lol sizeof() because i am of the dumb

--- unix_shell/BJS_wsh.c
+++ unix_shell/BJS_wsh.c
@@ -18,29 +18,26 @@ int main(void)
 	char *args[M_LIN/2 + 1]; // args go here
 	char buf[M_LIN]; // complete command goes here
 	int run = 1;
-	int bg = 0;
 	
 	// loop till we kill it
 	while (run) {
 		int i;
 		int argc = -1;
-		
+      int bg = 0;
 		
 		printf("wsh> "); // wsh = weird shell
 		fflush(stdout);
 		
 		fgets(buf, M_LIN, stdin); // read the command
 				
-		for(i=0;i<strlen(buf);i++) {
-			if (buf[i] == 'r') bg = 1;
+		for(i=0;i<sizeof(buf);i++) {
+			if (buf[i] == '&') bg = 1;
 			if(isspace(buf[i])){
 				 buf[i] = '\0';
-				 if (!i==strlen(buf)-1) args[++argc]=&buf[i+1];
-			}
+				 if (!i==sizeof(buf)-1) args[++argc]=&buf[i+1];
+			}      
 		}
-		
-		char *back = (bg == 1) ? "yes" : "no"; 
-		printf("%s : %s\n",buf,back);
 	}
+
 	return 0;
 }
