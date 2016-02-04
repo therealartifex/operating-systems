ea5e84cc53df5d6c9442b79d2c8cc49075ddc308
Sceleris Artifex <linux.usaf@live.com>
2016-02-03 22:02:22 -0600

    work on detecting ampersand

--- unix_shell/BJS_wsh.c
+++ unix_shell/BJS_wsh.c
@@ -18,27 +18,29 @@ int main(void)
 	char *args[M_LIN/2 + 1]; // args go here
 	char buf[M_LIN]; // complete command goes here
 	int run = 1;
+	int bg = 0;
 	
 	// loop till we kill it
 	while (run) {
 		int i;
-		int argc;
+		int argc = -1;
+		
 		
 		printf("wsh> "); // wsh = weird shell
 		fflush(stdout);
 		
 		fgets(buf, M_LIN, stdin); // read the command
-		
-		args[0]=&buf[0]; // first command is first argument
-		
+				
 		for(i=0;i<strlen(buf);i++) {
+			if (strcmp(buf[i], '&') == 0) bg = 1;
 			if(isspace(buf[i])){
 				 buf[i] = '\0';
-				 args[++argc]=&buf[i+1];
+				 if (!i==strlen(buf)-1) args[++argc]=&buf[i+1];
 			}
 		}
-			
-		printf("%s\n",buf);
+		
+		char *back = (bg == 1) ? "yes" : "no"; 
+		printf("%s : %s\n",buf,back);
 	}
 	return 0;
 }
