b40c2fc0b905c0833526352999975d9808edcaab
therealartifex <linux.usaf@live.com>
2016-02-07 20:50:12 -0600

    no seriously kill me

--- unix_shell/BJS_wsh.c
+++ unix_shell/BJS_wsh.c
@@ -3,20 +3,18 @@ Brian Scott
 COSC 3053-01
 Assignment 3 - "Simple UNIX Shell" 
 
-03 Feb 2016
+07 Feb 2016
 */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <ctype.h>
 #include <sys/types.h>
 
 #define M_LIN 128
 
 int main(void)
 {
-   char *args[M_LIN/2 + 1] = {NULL}; // args go here
 	char buf[M_LIN]; // complete command goes here
 	int run = 1;
 	
@@ -25,6 +23,7 @@ int main(void)
 		int i;
 		int arg_next = 0;
       int bg = 0;
+      char *args[M_LIN/2 + 1] = {NULL}; // args go here
 		
 		printf("wsh> "); // wsh = weird shell
 		fflush(stdout);
@@ -35,7 +34,10 @@ int main(void)
 		args[0] = &buf[0];
 
 		for(i=0;i<len;i++) {
-			if (buf[i] == '&') bg = 1;
+			if (buf[i] == '&') {
+            bg = 1;
+            buf[i] = '\0';
+         }
 			if(isspace(buf[i])){
 				 buf[i] = '\0';
 				 if (!(i==len-1)) args[++arg_next]=&buf[i+1]; // when we hit whitespace, set the next pointer in args to the next char in buf
@@ -47,13 +49,16 @@ int main(void)
       pid_t pid = fork();
       if (pid < 0) {
          // an error has occurred
-         fprintf(stderr, "Fork failed\n");
+         fprintf(stderr, "Fork failed for a reason unbeknownst to me\n");
          return 1;
       } else if (pid == 0) {
+         // child process
          execvp(args[0], args);
       } else {
+         // parent process
          if (bg==0) wait(NULL);
       }
+
 	}
 
 	return 0;
