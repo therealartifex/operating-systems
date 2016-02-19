941fad3bc8037f2c603d86d59c3ea21d9b22760e
Sceleris Artifex <linux.usaf@live.com>
2016-02-18 22:11:47 -0600

    fixed a bunch of crap

--- thread_stat/BJS_stat.c
+++ thread_stat/BJS_stat.c
@@ -4,37 +4,42 @@
 
 static float min, max, avg;
 
-void *average(float args[])
+void *average(void* p)
 {}
 
-void *max(float args[])
+void *maximum(void* p)
 {}
 
-void *min(float args[])
+void *minimum(void* p)
 {}
 	
 int main(int argc, char *argv[])
 {
      pthread_t thread1, thread2, thread3;
-     int iret1, iret2, iret3;
+     int iret1, iret2, iret3, i;
+     
+     float fl_args[argc-1];
+     for (i = 0; i<argc; i++){
+		 fl_args[i]=atof(argv[i+1]);
+	 }
  
     /* Create independent threads each of which will execute function */
  
-     iret1 = pthread_create( &thread1, NULL, min, (void*) message1);
+     iret1 = pthread_create( &thread1, NULL, minimum, (void*) fl_args);
      if(iret1)
      {
          fprintf(stderr,"Error - pthread_create() return code: %d\n",iret1);
          exit(EXIT_FAILURE);
      }
  
-     iret2 = pthread_create( &thread2, NULL, max, (void*) message2);
+     iret2 = pthread_create( &thread2, NULL, maximum, (void*) fl_args);
      if(iret2)
      {
          fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
          exit(EXIT_FAILURE);
      }
      
-     iret3 = pthread_create( &thread3, NULL, average, (void*) message2);
+     iret3 = pthread_create( &thread3, NULL, average, (void*) fl_args);
      if(iret3)
      {
          fprintf(stderr,"Error - pthread_create() return code: %d\n",iret3);
