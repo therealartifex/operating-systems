b23138d6213f0dea61956c815364334ae77d533a
Sceleris Artifex <linux.usaf@live.com>
2016-02-23 23:49:42 -0600

    fixed segfault in args parsing

--- philosopher/BJS_phil.c
+++ philosopher/BJS_phil.c
@@ -11,9 +11,9 @@
 #include <string.h>
 #include <time.h>
 
-#define PH 5
-#define LEFT (i+PH-1)%PH
-#define RIGHT (i+1)%PH
+#define PH 6 // number of philosophers
+#define LT (i+PH-1)%PH // left of current
+#define RT (i+1)%PH // right of current
 #define TK 1 // thinking
 #define HG 2 // hungry
 #define ET 3 // eating
@@ -23,67 +23,59 @@
 pthread_cond_t CV[PH];
 pthread_mutex_t M, output_lock; // the output_lock mutex circumvents annoying stdout interleaving
 int state[PH]; // state array
+int n, rounds = 0; // number of rounds to run 
+int eat_count[PH]; // number of times each philosopher has eaten
 
 int update_state(int i) {
-	if (state[i] == HG && state[LEFT] != ET && state[RIGHT] != ET) {
-		state[i] = ET;
-		pthread_cond_signal(&CV[i]);
-	}
-	return 0;
+    if (state[i] == HG && state[LT] != ET && state[RT] != ET) {
+        state[i] = ET;
+        pthread_cond_signal(&CV[i]);
+    }
+    return 0;
 }
 
 void chopsticks_init() {
-	int i;
-	pthread_mutex_init(&M, NULL);
+    int i;
+    pthread_mutex_init(&M, NULL);
 
-	for (i = 0; i < PH; i++) {
-		pthread_cond_init (&CV[i], NULL);
-		state[i] = TK;
-	}
+    for (i = 0; i < PH; i++) {
+        pthread_cond_init (&CV[i], NULL);
+        state[i] = TK;
+    }
 }
 
 void chopsticks_take(int i) {
-	lock(&M);
-	state[i] = HG;
-	update_state(i);
-	while (state[i] == HG) pthread_cond_wait (&CV[i],&M); // loop while hungry
-	unlock(&M);
+    lock(&M);
+    state[i] = HG;
+    update_state(i);
+    while (state[i] == HG) pthread_cond_wait (&CV[i],&M); // loop while hungry
+    unlock(&M);
 }
 
 void chopsticks_put(int i) {
-	lock(&M);
-	state[i] = TK;
-	update_state (LEFT);
-	update_state (RIGHT);
-	unlock(&M);
+    lock(&M);
+    state[i] = TK;
+    update_state (LT);
+    update_state (RT);
+    unlock(&M);
 }
-
-int nsteps, maxsteps = 0;  /* number of steps to run this test */
-int eat_count[PH];          /* number of steps for each thread */
-
-/* The subprogram trace is for testing and debugging.
-*    It allows us to see what is happening,
-*       and to shut down the philosophers when the program has
-*          run long enough for us to see that it seems to be working
-*           */
 
 void trace(int i, char *s) {
    // i = philospher ID
    // s = message
-	
+    
    lock(&output_lock);
-	if (strcmp (s, "eating") == 0) eat_count [i]++;
+   if (strcmp (s, "eating") == 0) eat_count [i]++;
    //   fprintf(stdout,"%d: %s\n",i,s);
-	
-   if (nsteps++ > maxsteps) {
-		if (strcmp(s,"thinking") == 0) {
-			unlock(&output_lock);
-         // fprintf (stderr, "thread done\n");
-			pthread_exit(0);
-		}
-	}
-	
-   unlock(&output_lock);
+    
+   if (n++ > rounds) {
+        if (strcmp(s,"thinking") == 0) {
+            unlock(&output_lock);
+            // fprintf (stderr, "thread done\n");
+            pthread_exit(0);
+        }
+    } 
+    unlock(&output_lock);
 }
 
 void * phil_exec(void *arg) {
@@ -98,12 +90,15 @@ void * phil_exec(void *arg) {
 
 int main(int argc, char *argv[]) {
    int i;
-   pthread_t tid[PH]; // IDs of the philospher threads
    int no[PH]; // corresponding table position numbers
+   pthread_t tid[PH]; // IDs of the philospher threads
    pthread_attr_t attr;
    
-   if (argc < 2 && atoi(argv[1]) == 0) maxsteps = 4;
-   else maxsteps = atoi(argv[1]);
+   // set rounds with command-line args, default to 4
+   if (argc < 2 || atoi(argv[1]) == 0) rounds = 4;
+   else rounds = atoi(argv[1]);
+
+   printf("Will run %d rounds\n", rounds);
 
    for (i = 0; i < PH; i++) eat_count[i] = 0;
 
@@ -111,7 +106,7 @@ int main(int argc, char *argv[]) {
 
    chopsticks_init(); // initialize chopsticks
    pthread_attr_init(&attr); // set default attributes
-   pthread_setconcurrency(4); // suggest four kernel threads for this process
+   pthread_setconcurrency(PH); // suggest PH kernel threads for this process
    pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM); // set system-wide contention scope
 
    // start up the philosopher threads
@@ -122,7 +117,6 @@ int main(int argc, char *argv[]) {
 
    // wait for threads to finish
    for (i = 0; i < PH; i++) pthread_join(tid[i], NULL);
-
    for (i = 0; i < PH; i++) fprintf (stderr, "philospher %d ate %d times\n", i, eat_count [i]);
 
    return 0;
