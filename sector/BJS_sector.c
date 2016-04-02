dfe2a6dbfef99ac89c7309767c5996c82d2916f5
therealartifex <linux.usaf@live.com>
2016-04-01 20:46:03 -0500

    add disk sector project

---
+++ sector/BJS_sector.c
@@ -0,0 +1,21 @@
+/* Brian Scott
+ * COSC 3603-01
+ * 01 Apr 2016
+ * Disk scheduling algorithms
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
+
+void *fcfs(void *p) {
+
+}
+
+int main() {
+   char buf[128];
+   fgets(buf, 128, stdin);
+   printf("DATA:\n%s",buf);
+   return 0;
+}
