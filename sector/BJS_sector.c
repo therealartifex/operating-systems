1d90620e133faecfe3f26ff740c2784c11b70b6c
therealartifex <linux.usaf@live.com>
2016-04-01 21:48:25 -0500

    literally sick of this crap

--- sector/BJS_sector.c
+++ sector/BJS_sector.c
@@ -1,21 +1,30 @@
 /* Brian Scott
- * COSC 3603-01
- * 01 Apr 2016
- * Disk scheduling algorithms
- */
+* COSC 3603-01
+* 01 Apr 2016
+* Disk scheduling algorithms
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <pthread.h>
+#include <string.h>
+
+static int sectors[1001];
 
 void *fcfs(void *p) {
 
 }
 
 int main() {
-   char buf[128];
-   fgets(buf, 128, stdin);
-   printf("DATA:\n%s",buf);
+   int i;
+   char buf[1024];
+   
+   while (1) {
+      sectors[i] = atoi(buf);
+      i++;
+   }
+   
    return 0;
 }
+
