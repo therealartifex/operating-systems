21f7ebd818ab091bc4b573b55e1f8bd444988b95
therealartifex <linux.usaf@live.com>
2016-04-02 18:43:57 -0500

    read stdin input and set up functions

--- sector/BJS_sector.c
+++ sector/BJS_sector.c
@@ -10,21 +10,43 @@
 #include <pthread.h>
 #include <string.h>
 
-static int sectors[1001];
+static int fcfs_total, sstf_total, scan_total, cscan_total, look_total, clook_total;
 
 void *fcfs(void *p) {
 
 }
 
+void *sstf(void *p) {
+
+}
+
+void *scan(void *p) {
+
+}
+
+void *cscan(void *p) {
+
+}
+
+void *look(void *p) {
+
+}
+
+void *clook(void *p) {
+
+}
+
 int main() {
-   int i;
-   char buf[1024];
+   int i, j, sectors[1001];
+   pthread_t t1, t2, t3, t4, t5, t6;
    
-   while (1) {
-      sectors[i] = atoi(buf);
-      i++;
+   while(scanf("%d", &i) == 1) {
+      sectors[j] = i;
+      j++;
    }
-   
+
+
+
    return 0;
 }
 
