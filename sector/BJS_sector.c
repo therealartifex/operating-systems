97b9b3ecb7bda186955db5167c68f55c4670531b
therealartifex <linux.usaf@live.com>
2016-04-02 19:27:36 -0500

    fine i will do it your way for now

--- sector/BJS_sector.c
+++ sector/BJS_sector.c
@@ -65,13 +65,7 @@ int main() {
    ptj(t5, NULL);
    ptj(t6, NULL);
 
-   printf("Total head movement for each disk scheduling algorithm:\n
-         FCFS:\t%d\n
-         SSTF:\t%d\n
-         SCAN:\t%d\n
-         C-SCAN\t%d\n
-         LOOK:\t%d\n
-         C-LOOK:\t%d\n");
+   printf("Total head movement for each disk scheduling algorithm:\nFCFS:\t%d\nSSTF:\t%d\nSCAN:\t%d\nC-SCAN\t%d\nLOOK:\t%d\nC-LOOK:\t%d\n",fcfs_total, sstf_total, scan_total, cscan_total, look_total, clook_total);
 
    return 0;
 }
